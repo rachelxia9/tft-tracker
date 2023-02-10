2dfd022de9d57e844b7008949165216d1cbff161
rachelxia <rachelx9@students.cs.ubc.ca>
2023-02-10 12:40:22 -0800

    todo: add specs

--- src/main/model/MatchHistory.java
+++ src/main/model/MatchHistory.java
@@ -20,23 +20,30 @@ public class MatchHistory {
         games.clear();
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds given game to match history and increments associated id
     public void addGame(Game g) {
         id++;
         games.put(id, g);
     }
 
+    // MODIFIES: this
+    // EFFECTS: removes game from match history using given id
     public void removeGame(int id) {
         games.remove(id);
     }
 
+    // EFFECTS: uses given id to access associated game in match history
     public Game accessGame(int id) {
         return games.get(id);
     }
 
+    // EFFECTS: return number of games in match history
     public int getNumGames() {
         return games.size();
     }
 
+    // EFFECTS: return percentage of winning games in match history, return 0 if empty
     public int getWinRate() {
         double count = 0;
         double numGames = this.getNumGames();
@@ -55,6 +62,8 @@ public class MatchHistory {
 
     }
 
+    // REQUIRES: valid rank [1-8]
+    // EFFECTS: return all games with given rank
     public int getGamesByRank(int rank) {
         int count;
         count = 0;
@@ -68,6 +77,8 @@ public class MatchHistory {
 
     }
 
+    // REQUIRES: non-empty string
+    // EFFECTS: return all games with given comp
     public int getGamesByComp(String comp) {
         int count;
         count = 0;
@@ -80,11 +91,12 @@ public class MatchHistory {
         return count;
     }
 
+    // EFFECTS: gets all games in match history without returning ids
     public HashMap<Integer, Game> getGames() {
         return games;
     }
 
-    // EFFECTS: returns id key of corresponding game
+    // EFFECTS: returns id key of corresponding game, returns -1 if not found
     public int getID(Game g) {
         for (Entry<Integer, Game> entry : games.entrySet()) {
             if (entry.getValue() == g) {
